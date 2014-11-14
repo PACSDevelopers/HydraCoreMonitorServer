586ff219d4cd4bfd6593a7c9246aa4fb139116a8
Ryan Howell <ryan@theryanhowell.co.uk>
2014-11-14 10:55:05 +0000

    Checking for file existance

--- settings/include.hh
+++ settings/include.hh
@@ -1,4 +1,7 @@
 <?hh
     // This file is the entry point for your settings, require more files here for the settings to be applied
-    require_once(__DIR__ . '/server.hh');
     require_once(__DIR__ . '/application.hh');
+    if(file_exists(__DIR__ . '/server.hh')) {
+        require_once(__DIR__ . '/server.hh');
+    }
+    
