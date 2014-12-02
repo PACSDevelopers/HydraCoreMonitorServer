02eced443732af5d8bcde94cd85acc3144610584
Ryan Howell <ryan@theryanhowell.co.uk>
2014-12-02 14:35:02 +0000

    Flexbox oh god why

--- public/data/exports/schemaExport.hh
+++ public/data/exports/schemaExport.hh
@@ -37,7 +37,7 @@ class SchemaExportPage extends \HC\Page {
 
         $this->settings['views']['header']['pageName'] .= $database->title . ' - ' . ucfirst($GET['name']);
 
-        $templates = <select id="templateSelect" class="form-control"><option value="0">None</option></select>;
+        $templates = <select id="templateSelect" class="form-control"><option value="0" selected="selected" disabled="disabled">Please select a template</option></select>;
         
         $db = new \HC\DB();
         $result = $db->read('data_templates', ['id', 'title'], ['status' => 1]);
@@ -55,13 +55,16 @@ class SchemaExportPage extends \HC\Page {
                 </div>
                 <div class="row">
                     <div class="form-group">
+                        <input type="hidden" id="databaseID" name="databaseID" value={$GET['id']} />
                         {$templates}
                     </div>
                 </div>
                 <div class="row" id="template">
-                    <ul class="list-group row" id="tableRowList"></ul>
+                    <ul class="list-group row flexbox-container" id="tableRowList"></ul>
                     <div class="cleafix"></div>
-                    <button class="btn btn-lg btn-primary btn-block" type="button" onclick="runExport();">Export</button>
+                    <div id="alertBox"></div>
+                    <div class="cleafix"></div>
+                    <button class="btn btn-lg btn-primary btn-block" type="button" onclick="runExport();">Schedule Export</button>
                 </div>
             </div>
         </x:frag>;
--- public/resources/.js/data/exports/export.js
+++ public/resources/.js/data/exports/export.js
@@ -23,15 +23,18 @@ function loadTemplate(id) {
 
                 setTimeout(function(){
                     console.log(response);
+                    window.selectedTables = [];
                     response.result.forEach(function(value){
                         var labelRow = '<li class="list-group-item col-xs-12 col-md-6 col-lg-3" id="tableRow' +  value['id'] + '">';
-                        labelRow += '<div class="pull-left text-left">' + value['name'] + '</div>';
+                        labelRow += '<div class="pull-left" style="max-width: 170px; overflow-wrap: break-word;"><p>' + value['alias'] + '</p></div>';
                         labelRow += '<div class="pull-right text-right"><input type="checkbox" data-toggle="toggle" class="form-checkbox checkbox-inline tableRowCheckbox" data-id="'+  value['id'] + '" id="tableRowCheckbox' +  value['id'] + '" /></div>';
                         labelRow += '<div class="cleafix"></div>';
                         labelRow += '</li>';
                         $template.append(labelRow);
-                        $('.tableRowCheckbox').bootstrapToggle();
                     });
+
+                    $('.tableRowCheckbox').bootstrapToggle();
+                    $alertBox.html(bootstrapAlert('success', 'Loaded template.'));
 
                 }, 1000);
             }
@@ -40,10 +43,54 @@ function loadTemplate(id) {
             // Tell user error
             $alertBox.html(bootstrapAlert('danger', 'Something went wrong, please try again.')).slideDown();
         });
-    
+
+}
+
+function runExport() {
+    var $alertBox = $('#alertBox');
+    var data = {'templateID': $('#templateSelect').val(), 'databaseID': $('#databaseID').val(), 'selectedTables': window.selectedTables};
+
+    $alertBox.html(bootstrapAlert('info', 'Scheduling export.')).slideDown();
+
+    $.ajax({
+        type: "POST",
+        url: '/ajax/exports/export/schedule',
+        data: {
+            data: data
+        }
+    })
+        .done(function(response) {
+            if (response.status) {
+                $alertBox.html(bootstrapAlert('success', 'Scheduled export.'));
+            } else {
+                $alertBox.html(bootstrapAlert('danger', 'Something went wrong, please try again.')).slideDown();
+            }
+        })
+        .fail(function() {
+            // Tell user error
+            $alertBox.html(bootstrapAlert('danger', 'Something went wrong, please try again.')).slideDown();
+        });
+
+}
+
+function bindEvents() {
+    $(document).on('change', '.tableRowCheckbox', function(){
+        var $this = $(this);
+        var value = $this.is(':checked');
+        var id = $this.attr('data-id');
+        if(value) {
+            window.selectedTables.push(id);
+        } else {
+            var index = window.selectedTables.indexOf(id);
+            if (index > -1) {
+                window.selectedTables.splice(index, 1);
+            }
+        }
+    });
 }
 
 $(document).ready(function(){
+    bindEvents();
     $(document).on('change', '#templateSelect', function(){
         loadTemplate($(this).val());
     });
